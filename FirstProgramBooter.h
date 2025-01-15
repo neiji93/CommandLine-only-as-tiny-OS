3ac46d337d64875a8a5a85cbaebf932463e2828b
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-15 10:06:42 +0100

    Create FirstProgramBooter.h

---
+++ FirstProgramBooter.h
@@ -0,0 +1,31 @@
+//see my crypto currency project 1 first
+
+
+
+class FractalsForComputerVisionBooterStackTrace
+{
+
+
+}
+
+//we will had to keep some iterative algorihtms
+class MemoryManagementComputerVisionStackTrace
+{
+
+
+}
+
+//As in crypto currency project 1, I wrote some internal programs, for command line only, we will
+//have to stop many processes in Realtime. It can be seen as cyber defense. Then, here, we can use 
+//Laurent lafforque researches ! Maybe we wont have to turn them to physics. In the futur, I hope to 
+//use something different from heat transfer. 
+class StopImmediateComplexProgramsProcesses
+{
+
+public:
+
+  void createIterativeRiemannSeriesProcessesForBlanckMemoryRAM();
+  void createIterativeRiemannSeriesProcessesForBlanckMemoryHD():
+
+}
+
